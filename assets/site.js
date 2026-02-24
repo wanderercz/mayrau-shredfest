7f6592e735edfc4f6f4de3438b5b9c4781c1d7d6
wanderercz <110418302+wanderercz@users.noreply.github.com>
2026-02-24 15:38:48 +0100

    Update site.js

--- assets/site.js
+++ assets/site.js
@@ -1,1 +1,8 @@
-
+// zvýrazní aktuální stránku v menu
+(function(){
+  const path = location.pathname.split('/').pop() || 'index.html';
+  document.querySelectorAll('nav a').forEach(a => {
+    const href = a.getAttribute('href');
+    if(href === path) a.style.color = 'white';
+  });
+})();
